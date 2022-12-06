915ad35222cee98b644e736bc80df76329df9649
codethi <docencia.thiago@gmail.com>
2022-12-06 20:27:10 -0300

    feat: connection pg and getall receitas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import express from "express";
+import pkg from "pg";
+
+const { Pool } = pkg;
+
+const connection = new Pool({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "root",
+  database: "tastecamp",
+});
 
 const app = express();
+
+app.get("/receitas", async (req, res) => {
+  const receitas = await connection.query("SELECT * FROM receitas;");
+  res.send(receitas.rows);
+});
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in port ${port}`));
